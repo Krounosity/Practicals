c066267d0900ad51e69aa3a622f39412fb52d34a
Varnan Rathod <119997446+Krounosity@users.noreply.github.com>
2023-10-07 11:02:22 +0530

    Added Practical5_b.c
    Added the completed and checked code of Practical5_b.c

---
+++ PPS/Practical5_b.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	int side;
+	int sum=0;
+	printf("Welcome to the lower triangular matrix creator! Please enter the length of the side of your matrix: ");
+	scanf("%d",&side);
+	int matrix[side-1][side-1];
+	for(int i = 0; i<side; i++){
+		for(int j = 0; j<=i; j++){
+			printf("Please enter element with index %d,%d: ",i+1,j+1);
+			scanf("%d",&matrix[i][j]);
+		}
+	}
+	for(int k = 0; k<side; k++){
+		sum = sum + 	matrix[k][k];
+	}
+	printf("The sum of the diagonal elements of your matrix is %d.\n",sum);
+	return 0;
+}
