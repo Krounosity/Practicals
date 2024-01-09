12e2f48947b1c2abe16856ced6ba13a9ed86c66f
Varnan A. Rathod <varnanrathod6@gmail.com>
2024-01-09 13:02:55 +0530

    Added practical 1 CWS in sem 2 folder.

---
+++ Sem2/practical_1/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+        <a
+          className="App-link"
+          href="https://github.com/Krounosity"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React with Varnan
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
